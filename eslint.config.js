3aa05833d3fb627e4a50af67d96eff80601e64f0
Evans Kofi Nyamekye <nyamekye2131@gmail.com>
2025-02-18 14:47:43 +0000

    Initial Commit

---
+++ eslint.config.js
@@ -0,0 +1,16 @@
+module.exports = [
+    {
+      ignores: ["node_modules/", "dist/"],
+    },
+    {
+      languageOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+      rules: {
+        "no-unused-vars": "warn",
+        "no-console": "off",
+      },
+    },
+  ];
+  
